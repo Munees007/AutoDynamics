2ab7d6e0397448e2b1eb9a060f91b966429ae147
Munees <muneesm458@gmail.com>
2025-05-23 23:14:28 +0530

    stock bug solved

---
+++ AutoDynamics.Shared/Modals/Stock/StockLogType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoDynamics.Shared.Modals.Product;
+namespace AutoDynamics.Shared.Modals.Stock
+{
+    public class StockLogType
+    {
+        public ProductType Product { get; set; } = new();
+
+    }
+}
